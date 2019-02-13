c1654c8ae2ac5c261903f3aa36f011754fb13cc4
Caio Casa Center <caiorsantanna@gmail.com>
2019-02-13 12:54:55 -0200

    commit inicial

---
+++ script.js
@@ -0,0 +1,19 @@
+$.trumbowyg.svgPath = false;
+$.trumbowyg.hideButtonTexts = true;
+$('#ctl00_Conteudo_tbxTexto_txtId').trumbowyg({
+	// btns: 
+	// [
+	//     ['viewHTML'],
+	//     ['undo', 'redo'], // Only supported in Blink browsers
+	//     ['formatting'],
+	//     ['strong', 'em', 'del'],
+	//     ['superscript', 'subscript'],
+	//     ['link'],
+	//     ['insertImage'],
+	//     ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+	//     ['unorderedList', 'orderedList'],
+	//     ['horizontalRule'],
+	//     ['removeformat']
+	// ]
+});
+
